96a82071ad0260de9baba40e3e58bc0d5828b418
AliDehghani <alidehghani8400@gmail.com>
2026-07-01 21:55:24 +0330

    Create constants

---
+++ src/Consts.hpp
@@ -0,0 +1,83 @@
+#ifndef CONSTS_HPP
+#define CONSTS_HPP
+
+#include <string>
+
+using namespace std;
+
+const string EMPTY = "Empty";
+const string OK = "OK";
+
+const string EQUAL = "equal";
+const string LATER = "later";
+const string EARLIER = "earlear";
+
+
+const string GET = "GET";
+const string PUT = "PUT";
+const string POST = "POST";
+const string DELETE = "DELETE";
+
+
+const string BAD_REQUEST = "Bad Request";
+const string PERMISSION_DENIED = "Permission Denied";
+const string NOT_FOUND = "Not Found";
+const string OVERLAP = "Overlap";
+const string HOLIDAY_FOUND = "Holiday Found";
+
+
+const string SIGNUP = "signup";
+const string LOGIN = "login";
+const string LOGOUT = "logout";
+const string EVENT = "event";
+const string PERIODIC_EVENT = "periodic_event";
+const string JOIN_EVENT = "join_event";
+const string CONFIRM_JOIN_EVENT = "confirm_join_event";
+const string REJECT_JOIN_EVENT = "reject_join_event";
+const string TASK = "task";
+const string REPORT = "report";
+
+const string USERNAME ="username";
+const string PASSWORD = "password";
+
+const string DATE = "date";
+const string START_DATE = "start_date";
+const string END_DATE = "end_date";
+const string TYPE = "type";
+const string START_TIME = "start_time";
+const string END_TIME = "end_time";
+const string DURATION = "duration";
+const string TITLE = "title";
+const string DESCRIPTION = "description";
+const string WEEK_DAYS = "week_days";
+
+const string DAY = "day";
+const string FROM = "from";
+const string TO = "to";
+
+const string GUESTS = "guests";
+
+
+const string INVITATION_ID = "invitation_id";
+const string TIME = "time";
+const string TASK_ID = "task_id";
+
+
+const string NORMAL = "Bormal";
+const string DAILY = "Daily";
+const string WEEKLY = "Weekly";
+const string MONTHLY = "Monthly";
+
+const string SATURDAY = "Saturday";
+const string SUNDAY = "Sunday";
+const string MONDAY = "Monday";
+const string TUESDAY = "Tuesday";
+const string WEDNESDAY = "Wednesday";
+const string THURSDAY = "Thursday";
+const string FRIDAY = "Friday";
+
+const string FIRST_DAY = "1404/01/01";
+
+const string REPLACE = "replace";
+
+#endif
